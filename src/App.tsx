b27246cd291af141467952fb934ed567730dc191
Vladyslav Koponkin <vladyslav.k@collectius.com>
2023-09-23 20:50:24 +0100

    initial

---
+++ src/App.tsx
@@ -0,0 +1,132 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+        <div className="container">
+            <div className="code__container">
+                <div className="code__head">
+                    <span>Resume</span>
+                    <div className="code__controls">
+                    </div>
+                </div>
+
+                <div className="code__body">
+                    Vladyslav K.
+                    <br />
+                    Frontend Developer
+                    <br />
+                    <br />
+                    Summary:
+                    <br />
+                    --------------------------
+                    <br />
+                    I'm a seasoned Senior Frontend Developer with over 7 years of hands-on experience in the full software development lifecycle. My expertise lies in designing, developing, testing, and deploying both web and mobile applications. I excel in harnessing the power of JavaScript libraries and frameworks, particularly React and Vue, to successfully deliver projects of varying complexities. I am known for my positive attitude and the ability to make sound decisions even in high-pressure situations.
+                    <br />
+                    <br />
+                    <br />
+                    Work Experience:
+                    <br />
+                    --------------------------
+                    <br />
+                    **Intersog**
+                    <br />
+                    June 2018 - Present
+                    <br />
+                    <br />
+                    *Project 1: GDPR Solution*
+                    <br />
+                    - Utilized HTML, CSS, JavaScript, React, Redux, Webpack, and RESTful APIs to develop a GDPR-related solution that processes personal information using artificial intelligence.
+                    <br />
+                    - Contributed to design discussions and built scalable, secure web applications.
+                    - Collaborated with UI/UX, QA, and backend teams.
+                    <br />
+                    - Developed new application modules and various UI components.
+                    <br />
+                    <br />
+                    *Project 2: Financial Management Platform*
+                    <br />
+                    - Assisted the backend development team by providing frontend solutions using JavaScript, TypeScript, HTML, CSS, React, and Vue.
+                    <br />
+                    - Developed frontend solutions using RESTful APIs and GraphQL.
+                    <br />
+                    - Translated web designs into functional frontend applications.
+                    <br />
+                    - Improved web app reliability through unit tests and automated testing.
+                    <br />
+                    - Contributed to configuring DevOps solutions for frontend application deployments.
+                    <br />
+                    <br />
+                    <br />
+                    **CyberVision**
+                    <br />
+                    August 2017 - May 2018
+                    <br />
+                    <br />
+                    - Worked on IoT projects, specializing in the development of visualization and interactive UI components using HTML, CSS, JavaScript, React, Redux, Angular 4+, RxJS, Webpack, and Git.
+                    <br />
+                    <br />
+                    <br />
+                    **LENAL**
+                    <br />
+                    July 2016 - July 2017
+                    <br />
+                    <br />
+                    - Organized and led the mobile development department.
+                    <br />
+                    - Implemented code rules and style guides.
+                    <br />
+                    - Successfully completed and maintained large-scale e-commerce projects using HTML, CSS, JavaScript, jQuery, Phonegap, Gulp, and Git.
+                    <br />
+                    <br />
+                    <br />
+                    **Freelance (Self-employed)**
+                    <br />
+                    November 2015 - July 2016
+                    <br />
+                    <br />
+                    - Developed web applications, websites, and digital solutions using HTML, CSS, JavaScript, jQuery, Gulp, and Git.
+                    <br />
+                    <br />
+                    <br />
+                    Skills:
+                    <br />
+                    --------------------------
+                    <br />
+                    - Languages: JavaScript, TypeScript, HTML/CSS
+                    <br />
+                    - Technologies: React.js, Redux, Node.js, Vue.js, React Native, jQuery
+                    <br />
+                    - CSS libraries: Bootstrap, Materialize, etc.
+                    <br />
+                    - Preprocessors: Jade (Pug), SASS/SCSS.
+                    <br />
+                    - VCS: GIT (GitHub, Bitbucket, GitLab etc…).
+                    <br />
+                    - Build tools: Webpack, Vite.
+                    <br />
+                    - SBS: Gulp.
+                    <br />
+                    - Experienced in integrating with RESTful APIs.
+                    <br />
+                    <br />
+                    <br />
+                    Education:
+                    <br />
+                    --------------------------
+                    <br />
+                    Bachelor of Science
+                    <br />
+                    Volodymyr Dahl East Ukrainian National University
+                    <br />
+                    Faculty of Information Technology and Electronics
+                </div>
+            </div>
+        </div>
+    </>
+  )
+}
+
+export default App
